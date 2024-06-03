70c06b7e3f66b7f94c242e67adfbf65d413786ae
sifat2626 <ashrafulsifat26@gmail.com>
2024-06-03 11:04:46 +0600

    filtering biodata logic done

--- src/pages/BioDatas.jsx
+++ src/pages/BioDatas.jsx
@@ -2,27 +2,63 @@ import { useQuery } from "@tanstack/react-query";
 import Filter from "../shared/Filter";
 import { axiosCommon } from "../hooks/useAxiosCommon";
 import Biodata from "../components/Biodata";
+import queryString from "query-string";
+import { useEffect, useState } from "react";
 
 function BioDatas() {
+  const [search, setSearch] = useState(location.search);
+  const parsed = queryString.parse(location.search);
+
+  const [biodataType, setBiodataType] = useState(parsed.biodataType || "");
+  const [division, setDivision] = useState(parsed.division || "");
+  const [minAge, setMinAge] = useState(parsed.minAge || "");
+  const [maxAge, setMaxAge] = useState(parsed.maxAge || "");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    parsed.minAge = minAge;
+    parsed.maxAge = maxAge;
+    parsed.biodataType = biodataType;
+    parsed.division = division;
+    const stringified = queryString.stringify(parsed);
+    setSearch(`?${stringified}`);
+  };
+
   const {
-    isPending,
+    isLoading,
     error,
     data: bioDatas = [],
+    refetch,
   } = useQuery({
-    queryKey: ["bio-datas"],
+    queryKey: ["bio-datas", search],
     queryFn: async () => {
-      const { data } = await axiosCommon("/biodatas");
+      const { data } = await axiosCommon(`/biodatas${search}`);
       return data?.biodatas;
     },
+    enabled: false, // Disable automatic refetching
   });
 
-  if (isPending) return "Loading...";
+  useEffect(() => {
+    refetch();
+  }, [search, refetch]);
 
+  if (isLoading) return "Loading...";
   if (error) return "An error has occurred: " + error.message;
+
   return (
     <div className="flex items-start">
       <div className="">
-        <Filter />
+        <Filter
+          setBiodataType={setBiodataType}
+          setDivision={setDivision}
+          setMinAge={setMinAge}
+          setMaxAge={setMaxAge}
+          biodataType={biodataType}
+          division={division}
+          maxAge={maxAge}
+          minAge={minAge}
+          handleSubmit={handleSubmit}
+        />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2">
         {bioDatas.map((biodata) => (
--- src/shared/Filter.jsx
+++ src/shared/Filter.jsx
@@ -1,37 +1,71 @@
-function Filter() {
+/* eslint-disable react/prop-types */
+function Filter({
+  handleSubmit,
+  minAge,
+  setMinAge,
+  maxAge,
+  setMaxAge,
+  biodataType,
+  setBiodataType,
+  division,
+  setDivision,
+}) {
   return (
     <div className="w-64 bg-red-200 min-h-screen">
-      <label htmlFor="">Age range</label>
-      <div className="flex items-center">
-        <div className="">
-          <input type="text" className="input input-bordered w-full max-w-xs" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="age-range">Age range</label>
+        <div className="flex items-center">
+          <div className="">
+            <input
+              type="text"
+              name="minAge"
+              className="input input-bordered w-full max-w-xs"
+              placeholder="min age"
+              value={minAge}
+              onChange={(e) => setMinAge(e.target.value)}
+            />
+          </div>
+          <p>-</p>
+          <div className="">
+            <input
+              type="text"
+              name="maxAge"
+              className="input input-bordered w-full max-w-xs"
+              placeholder="max age"
+              value={maxAge}
+              onChange={(e) => setMaxAge(e.target.value)}
+            />
+          </div>
         </div>
-        <p>-</p>
         <div className="">
-          <input type="text" className="input input-bordered w-full max-w-xs" />
+          <select
+            name="biodataType"
+            className="select select-bordered w-full max-w-xs"
+            value={biodataType}
+            onChange={(e) => setBiodataType(e.target.value)}
+          >
+            <option value="">Biodata type?</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
         </div>
-      </div>
-      <div className="">
-        <select className="select select-bordered w-full max-w-xs">
-          <option value={""} selected>
-            Biodata type?
-          </option>
-          <option value={"Male"}>Male</option>
-          <option value={"Female"}>Female</option>
+        <select
+          name="division"
+          className="select select-bordered w-full max-w-xs"
+          value={division}
+          onChange={(e) => setDivision(e.target.value)}
+        >
+          <option value="">Permanent Division?</option>
+          <option value="Dhaka">Dhaka</option>
+          <option value="Chattagram">Chattagram</option>
+          <option value="Rangpur">Rangpur</option>
+          <option value="Barisal">Barisal</option>
+          <option value="Khulna">Khulna</option>
+          <option value="Mymensingh">Mymensingh</option>
+          <option value="Sylhet">Sylhet</option>
         </select>
-      </div>
-      <select className="select select-bordered w-full max-w-xs">
-        <option value={""} selected>
-          Permanent Division?
-        </option>
-        <option value={"Dhaka"}>Dhaka</option>
-        <option value={"Chattagram"}>Chattagram</option>
-        <option value={"Rangpur"}>Rangpur</option>
-        <option value={"Barisal"}>Barisal</option>
-        <option value={"Khulna"}>Khulna</option>
-        <option value={"Mymensingh"}>Mymensingh</option>
-        <option value={"Sylhet"}>Sylhet</option>
-      </select>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
