2be382820e57feb4990f0d5f42b910d8b2bec29a
sifat2626 <ashrafulsifat26@gmail.com>
2024-06-10 14:58:03 +0600

    Update SuccessCounter.jsx

--- src/shared/SuccessCounter.jsx
+++ src/shared/SuccessCounter.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import useAxiosCommon from "../hooks/useAxiosCommon";
 import useAxiosSecure from "../hooks/useAxiosSecure";
+import CountUp from "react-countup";
 
 function SuccessCounter() {
   const [stats, setStats] = useState({});
@@ -25,21 +26,73 @@ function SuccessCounter() {
   console.log(stats);
   return (
     <div className=" bg-yellow-800 py-12 text-white mt-12 flex grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-[5%] justify-between">
-      <div className="text-center">
-        <h3 className="text-2xl font-bold mb-4">{count}</h3>
-        <p className="text-xl font-semibold">Successful Marriages</p>
+      <div className="text-center text-2xl font-medium">
+        <CountUp
+          start={0}
+          end={count}
+          delay={2}
+          duration={3}
+          enableScrollSpy
+          redraw={true}
+        >
+          {({ countUpRef }) => (
+            <div>
+              <span ref={countUpRef} />
+            </div>
+          )}
+        </CountUp>
+        <p className="text-xl font-semibold mt-4">Successful Marriages</p>
       </div>
-      <div className="text-center">
-        <h3 className="text-2xl font-bold mb-4">{stats.count}</h3>
-        <p className="text-xl font-semibold">Total Biodata</p>
+      <div className="text-center text-2xl font-medium">
+        <CountUp
+          start={0}
+          end={stats.count}
+          delay={2}
+          duration={3}
+          enableScrollSpy
+          redraw={true}
+        >
+          {({ countUpRef }) => (
+            <div>
+              <span ref={countUpRef} />
+            </div>
+          )}
+        </CountUp>
+        <p className="text-xl font-semibold mt-4">Total Biodata</p>
       </div>
-      <div className="text-center">
-        <h3 className="text-2xl font-bold mb-4">{stats.maleCount}</h3>
-        <p className="text-xl font-semibold">Male Biodata</p>
+      <div className="text-center text-2xl font-medium">
+        <CountUp
+          start={0}
+          end={stats.maleCount}
+          delay={2}
+          duration={3}
+          enableScrollSpy
+          redraw={true}
+        >
+          {({ countUpRef }) => (
+            <div>
+              <span ref={countUpRef} />
+            </div>
+          )}
+        </CountUp>
+        <p className="text-xl font-semibold mt-4">Male Biodata</p>
       </div>
-      <div className="text-center">
-        <h3 className="text-2xl font-bold mb-4">{stats.femaleCount}</h3>
-        <p className="text-xl font-semibold">Female Biodata</p>
+      <div className="text-center text-2xl font-medium">
+        <CountUp
+          start={0}
+          end={stats.femaleCount}
+          delay={2}
+          duration={3}
+          enableScrollSpy
+          redraw={true}
+        >
+          {({ countUpRef }) => (
+            <div>
+              <span ref={countUpRef} />
+            </div>
+          )}
+        </CountUp>
+        <p className="text-xl font-semibold mt-4">Female Biodata</p>
       </div>
     </div>
   );
